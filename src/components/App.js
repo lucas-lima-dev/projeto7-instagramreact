150eff9fd0b9c94633d7656f2c6bd1632715ca2a
lucas-lima-dev <lucas.sdslima@gmail.com>
2022-11-16 23:58:58 -0300

    Criaçao de cada componente React

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Corpo from "./Corpo"
+import NavBar from "./NavBar"
+
+export default function APP() {
+    return (
+        <div>
+            <NavBar/>
+            <Corpo/>
+        </div>
+    )
+
+}
