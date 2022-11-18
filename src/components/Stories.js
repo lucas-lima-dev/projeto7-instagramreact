3570f1a27e365e5fd0a7571a019a3769343e5330
lucas-lima-dev <lucas.sdslima@gmail.com>
2022-11-17 23:31:17 -0300

    Refatoracao dos stories usando maps componentes e props

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,16 +1,41 @@
 export default function Stories() {
-    // const list = [];
+  const story = [
+    { pageImg: "assets/img/9gag.svg", userName: "9gag" },
+    { pageImg: "assets/img/meowed.svg", userName: "meowed" },
+    { pageImg: "assets/img/barked.svg", userName: "barked" },
+    {
+      pageImg: "assets/img/nathanwpylestrangeplanet.svg",
+      userName: "nathanwpylestrangeplanet",
+    },
+    { pageImg: "assets/img/wawawicomics.svg", userName: "wawawicomics" },
+    { pageImg: "assets/img/respondeai.svg", userName: "respondeai" },
+    { pageImg: "assets/img/filomoderna.svg", userName: "filomoderna" },
+    { pageImg: "assets/img/memeriagourmet.svg", userName: "memeriagourmet" },
+  ];
 
   return (
     <div class="stories">
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/9gag.svg" />
-        </div>
-        <div class="usuario">9gag</div>
+      {story.map((s) => <Story pImg = {s.pageImg} uName = {s.userName}/>)}
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
       </div>
+    </div>
+  );
+}
 
-      <div class="story">
+function Story(props) {
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.pImg} />
+      </div>
+      <div class="usuario">{props.uName}</div>
+    </div>
+  );
+}
+
+{
+  /* <div class="story">
         <div class="imagem">
           <img src="assets/img/meowed.svg" />
         </div>
@@ -57,11 +82,5 @@ export default function Stories() {
           <img src="assets/img/memeriagourmet.svg" />
         </div>
         <div class="usuario">memeriagourmet</div>
-      </div>
-
-      <div class="setinha">
-        <ion-icon name="chevron-forward-circle"></ion-icon>
-      </div>
-    </div>
-  );
+      </div> */
 }
